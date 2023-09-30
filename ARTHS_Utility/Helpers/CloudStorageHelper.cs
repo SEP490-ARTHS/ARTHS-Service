ec381ebc9938c4293a2d264e56b06703de4902ab
Tấn Trung <nguyentantrung1801@gmail.com>
2023-09-30 16:56:46 +0700

    fix-storage

--- ARTHS_Utility/Helpers/CloudStorageHelper.cs
+++ ARTHS_Utility/Helpers/CloudStorageHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Storage.V1;
+using System.Reflection;
 
 namespace ARTHS_Utility.Helpers
 {
@@ -10,21 +11,44 @@ namespace ARTHS_Utility.Helpers
 
         static CloudStorageHelper()
         {
-            var basePath = AppDomain.CurrentDomain.BaseDirectory;
-            //thư mục gốc
-            var projectRoot = Path.GetFullPath(Path.Combine(basePath, "..", "..", "..", ".."));
-            string credentialPath = Path.Combine(projectRoot, "ARTHS_Utility", "Helpers", "CloudStorage", "arths-45678-firebase-adminsdk-plwhs-954089d6b7.json");
-            //if (!File.Exists(credentialPath))
-            //{
-            //    throw new FileNotFoundException($"File not found at {credentialPath}");
-            //}
-            var credential = GoogleCredential.FromFile(credentialPath);
+            var credentialJson = Environment.GetEnvironmentVariable("GoogleCloudCredential");
+            if (string.IsNullOrWhiteSpace(credentialJson))
+            {
+                throw new InvalidOperationException("Không thể tìm thấy cấu hình Google Cloud trong biến môi trường.");
+            }
 
+            var credential = GoogleCredential.FromJson(credentialJson);
             // Storage
             Storage = StorageClient.Create(credential);
 
             // Url Signer
             UrlSigner = UrlSigner.FromCredential(credential);
+
+            //try
+            //{
+            //    //var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            //    //thư mục gốc
+            //    //var projectRoot = Path.GetFullPath(Path.Combine(basePath, "..", "..", "..", ".."));
+            //    //string credentialPath = Path.Combine(projectRoot, "ARTHS_Utility", "Helpers", "CloudStorage", "arths-45678-firebase-adminsdk-plwhs-954089d6b7.json");
+            //    //if (!File.Exists(credentialPath))
+            //    //{
+            //    //    throw new FileNotFoundException($"File not found at {credentialPath}");
+            //    //}
+            //    var projectPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+            //    var credentialPath = Path.Combine(projectPath, "Helpers", "CloudStorage", "arths-45678-firebase-adminsdk-plwhs-954089d6b7.json");
+            //    var credential = GoogleCredential.FromFile(credentialPath);
+
+            //    // Storage
+            //    Storage = StorageClient.Create(credential);
+
+            //    // Url Signer
+            //    UrlSigner = UrlSigner.FromCredential(credential);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    // Hoặc sử dụng một hệ thống logging nếu bạn đã cài đặt
+            //}
         }
 
         public static StorageClient GetStorage()
