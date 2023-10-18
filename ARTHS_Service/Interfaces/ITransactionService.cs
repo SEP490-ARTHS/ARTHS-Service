f89128760b945923b31dc0cf17cfc87f69eafd6a
Tấn Trung <nguyentantrung1801@gmail.com>
2023-10-19 01:11:22 +0700

    hot fix payment result

--- ARTHS_Service/Interfaces/ITransactionService.cs
+++ ARTHS_Service/Interfaces/ITransactionService.cs
@@ -4,6 +4,7 @@ namespace ARTHS_Service.Interfaces
 {
     public interface ITransactionService
     {
+        Task<TransactionViewModel> GetTransaction(Guid id);
         Task<List<TransactionViewModel>> GetTransactions();
     }
 }
